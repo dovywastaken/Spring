ee3bd5f1e40e710dfcce696bc62a65544819a497
Euishin Kim <dovies401@gmail.com>
2024-11-13 17:58:46 +0900

    [chap02] [init] [intial commit of the project chap02]

---
+++ chap02/src/main/java/com/springmvc/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.springmvc.controller;
+
+import java.util.Date;
+import java.text.DateFormat;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller //컨트롤러 지정하기
+public class HomeController 
+{
+	
+	@RequestMapping(value ="/", method = RequestMethod.GET) //doGet과 같은 역할
+	public String home(Locale locale, Model model) 
+	{	
+	      Date date = new Date();
+	      DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+	      
+	      String formattedDate = dateFormat.format(date);
+	      
+	      model.addAttribute("serverTime", formattedDate );
+		
+		
+		// /WEB-IMF/views/    여기에 home이 들어감       .jsp
+		return "home";	
+	}
+}
