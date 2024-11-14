6fd5a2c30771d1b4a9dbfb6b533ea58ec68cc593
Euishin Kim <dovies401@gmail.com>
2024-11-14 14:41:50 +0900

    [Spring_BookMarket] [init] [initial commit of Spring_BookMarket project, Welcome page was added, web.xml, servlet-context.xml, pon.xml were edited]

--- chap02/src/main/java/com/springmvc/controller/HomeController.java
+++ chap02/src/main/java/com/springmvc/controller/HomeController.java
@@ -1,9 +1,11 @@
 package com.springmvc.controller;
 
 import java.util.Date;
+import java.util.Locale;
 import java.text.DateFormat;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -24,5 +26,13 @@ public class HomeController
 		
 		// /WEB-IMF/views/    여기에 home이 들어감       .jsp
 		return "home";	
+	}
+	
+	@RequestMapping(value = "/test")
+	public String test(Model model) 
+	{
+		String test = "안녕하세요";
+		model.addAttribute("tt",test);
+		return "test";
 	}
 }
